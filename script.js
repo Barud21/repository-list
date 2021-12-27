761937cfea1f42ebd34f1eb5ec75d29ddb33c7ec
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2021-12-28 00:13:12 +0100

    Using fetchAPI to get data from Github

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 "use strict";
 
-let repositoryList = [];
-
-function parseAPIObject() {
-  const responseObj = JSON.parse(this.responseText);
-  console.log(responseObj);
-  for (let i = 0; i < responseObj.length; i++) {
-    const repo = {
-      name: responseObj[i].name,
-      stars: responseObj[i].stargazers_count,
-    };
-    repositoryList.push(repo);
+async function fetchAPIObject(username) {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos`,
+      {
+        method: "GET",
+      }
+    );
+    const APIObject = await response.json();
+    return APIObject;
+  } catch (error) {
+    console.log(error);
   }
-  return responseObj;
-}
-
-function getAPIObject(username) {
-  let repoList = new XMLHttpRequest();
-  repoList.open("get", `https://api.github.com/users/${username}/repos`, true);
-  repoList.onload = parseAPIObject;
-  repoList.send();
 }
 
 function mapObjectToDTO(externalDTO) {
+  const internalDTO = [];
   for (let i = 0; i < externalDTO.length; i++) {
-    internalDTO.push(externalDTO[i]);
+    const repo = {
+      name: externalDTO[i].name,
+      stars: externalDTO[i].stargazers_count,
+    };
+    internalDTO.push(repo);
   }
+  return internalDTO;
 }
 
-const getObject = getAPIObject("allegro");
-console.log("API object:");
-console.log(repositoryList);
-console.log(repositoryList[2]); // returns undefined
-const internalDTO = mapObjectToDTO(repositoryList);
-console.log("internal DTO:");
+const externalAPIObject = await fetchAPIObject("allegro");
+console.log(externalAPIObject);
+
+const internalDTO = mapObjectToDTO(externalAPIObject);
 console.log(internalDTO);
